2a586aebc78e9f39e15d3714ac81650908c4dcda
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2024-05-29 13:02:02 +0000

    Improved logic and handling 500 errors in endpoint

--- api/vote.ts
+++ api/vote.ts
@@ -1,14 +1,28 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import { db } from '../config/firebaseConfig'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // If the request is different from a POST, return a 405 error
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' })
+  const { method } = req
+
+  switch (method) {
+    case 'POST':
+      try {
+        // TODO: Using the request body, update Firebase to add a new vote in votes collection, adding the vote result and timestamp
+        const { name = 'World' } = req.body
+        const docRef = db.collection('testCollection').doc('testDoc')
+        await docRef.set({ message: `Hello ${name}!` })
+        return res
+          .status(201)
+          .json({ message: 'Document created successfully' })
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: 'Internal Server Error', error: error.message })
+      }
+
+    // If the request is different from the selected ones , return 405 error
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'DELETE'])
+      return res.status(405).end(`Method ${method} Not Allowed`)
   }
-
-  // TODO: Using the request body, update Firebase to add a new vote in votes collection, adding the vote result and timestamp
-
-  return res.status(200).json({
-    message: 'Vote now',
-  })
 }
