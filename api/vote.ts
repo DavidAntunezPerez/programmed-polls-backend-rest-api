0269175b938bb2a6a00d3a9f881f07cca62e4d33
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2024-05-29 12:58:37 +0000

    Added prettier config file and added Method not allowed when calling vote

--- api/vote.ts
+++ api/vote.ts
@@ -1,7 +1,14 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    return res.status(200).json({
-        message: 'Vote now',
-      });
+  // If the request is different from a POST, return a 405 error
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' })
+  }
+
+  // TODO: Using the request body, update Firebase to add a new vote in votes collection, adding the vote result and timestamp
+
+  return res.status(200).json({
+    message: 'Vote now',
+  })
 }
