efbc9e6a929e49c6e0f06b3a7506654c24df0e0a
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2024-05-29 12:42:02 +0000

    Installed Vercel dependency and created basic endpoint vote

---
+++ api/vote.ts
@@ -0,0 +1,7 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    return res.status(200).json({
+        message: 'Vote now',
+      });
+}
