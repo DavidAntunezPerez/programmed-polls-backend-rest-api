8ea95e6d3ea06fe6acc4bd8f77b646e28f2cc2fd
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2024-05-30 10:03:21 +0000

    Removed hello endpoint, formatted vote endpoint

--- api/vote.ts
+++ api/vote.ts
@@ -8,19 +8,19 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     case 'POST':
       try {
         // TODO: Using the request body, update Firebase to add a new vote in votes collection, adding the vote result and timestamp
-        const { name = 'David' } = req.query;
-        const docRef = db.collection('votes').doc('vote');
+        const { name = 'David' } = req.query
+        const docRef = db.collection('votes').doc('vote')
         await docRef.set({
           message: `Voted by ${name}!`,
-        });
-    
+        })
+
         // Read the document from Firestore
-        const doc = await docRef.get();
+        const doc = await docRef.get()
         if (doc.exists) {
-          const data = doc.data();
+          const data = doc.data()
           return res.json({
             message: `Successfully wrote and read from Firestore: ${data?.message}`,
-          });
+          })
         }
         return res
           .status(201)
