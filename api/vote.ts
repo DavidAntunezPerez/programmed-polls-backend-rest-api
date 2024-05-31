de3e8b80525fbf50ed282d56a75d9ec29c8d5edc
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2024-05-31 14:37:40 +0000

    Improved POST vote

--- api/vote.ts
+++ api/vote.ts
@@ -2,38 +2,48 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { db } from '../config/firebaseConfig'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { method } = req
+  const { method, body } = req
 
   switch (method) {
     case 'POST':
       try {
-        // TODO: Using the request body, update Firebase to add a new vote in votes collection, adding the vote result and timestamp
-        const { name = 'David' } = req.query
-        const docRef = db.collection('votes').doc('vote')
-        await docRef.set({
-          message: `Voted by ${name}!`,
-        })
-
-        // Read the document from Firestore
-        const doc = await docRef.get()
-        if (doc.exists) {
-          const data = doc.data()
-          return res.json({
-            message: `Successfully wrote and read from Firestore: ${data?.message}`,
+        if (!body || !body.timestamp || !body.votes) {
+          return res.status(400).json({
+            message:
+              'Bad Request: Missing timestamp or votes field in request body',
           })
         }
-        return res
-          .status(201)
-          .json({ message: 'Document created successfully' })
+
+        const { timestamp, votes } = body
+        const formattedTimestamp = new Date(timestamp)
+
+        if (isNaN(formattedTimestamp.getTime())) {
+          return res
+            .status(400)
+            .json({ message: 'Bad Request: Invalid timestamp format' })
+        }
+
+        // Convert to GMT+2
+        const gmt2Offset = 2 * 60 * 60 * 1000
+        const gmt2Timestamp = new Date(
+          formattedTimestamp.getTime() + gmt2Offset,
+        )
+
+        const docRef = db.collection('votes').doc()
+        await docRef.set({
+          timestamp: gmt2Timestamp,
+          votes: votes,
+        })
+
+        return res.status(201).json({ message: 'Vote recorded successfully' })
       } catch (error) {
         return res
           .status(500)
           .json({ message: 'Internal Server Error', error: error.message })
       }
 
-    // If the request is different from the selected ones , return 405 error
     default:
-      res.setHeader('Allow', ['GET', 'POST', 'DELETE'])
+      res.setHeader('Allow', ['POST'])
       return res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
