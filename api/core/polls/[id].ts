0561b558191115bad8fafca93cf8a4dcf3c22d39
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2024-06-07 21:17:24 +0000

    Get by id is now functional

--- api/core/polls/[id].ts
+++ api/core/polls/[id].ts
@@ -1,8 +1,56 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import { db } from '../../../config/firebaseConfig'
+import authenticate from '../../../config/authenticate'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  await new Promise(resolve => authenticate(req, res, resolve))
   const { method, query } = req
+
+  // Get userId from the authenticated user and the poll ID
+  const userId = (req as any).user.uid
   const { id } = query
 
-  return res.status(200).json({ message: `Poll ID: ${id}` })
+  switch (method) {
+    case 'GET':
+      try {
+        // Access the polls collection and get the document with the given ID
+        const pollDoc = await db
+          .collection('polls')
+          .doc(id as string)
+          .get()
+
+        if (!pollDoc.exists) {
+          return res.status(404).json({ message: 'Poll not found' })
+        }
+
+        const pollData = pollDoc.data()
+
+        // Ensure that the poll belongs to the authenticated user
+        if (pollData?.userId !== userId) {
+          return res.status(403).json({
+            message: 'Forbidden, you do not have access to this poll.',
+          })
+        }
+
+        const response = {
+          duration: pollData?.duration,
+          isEnabled: pollData?.isEnabled,
+          options: pollData?.options,
+          description: pollData?.description,
+          title: pollData?.title,
+          frequency: pollData?.frequency,
+          createdAt: pollData?.createdAt.toDate().toISOString(),
+        }
+
+        return res.status(200).json(response)
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: 'Internal Server Error', error: error.message })
+      }
+
+    default:
+      res.setHeader('Allow', ['DELETE', 'GET', 'PATCH'])
+      return res.status(405).end(`Method ${method} Not Allowed`)
+  }
 }
