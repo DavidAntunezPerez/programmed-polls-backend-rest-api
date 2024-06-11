a141c09054ea2a1a5cd0f692b5c3f3b70933c6ff
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2024-06-11 11:49:54 +0000

    Fixed bug with Internal Server Error for not validating properly arrays and improved validation performance

--- utils/validation.ts
+++ utils/validation.ts
@@ -13,20 +13,25 @@ export const validate = (data: any, schema: Schema): ValidationResult => {
     }
 
     if (value !== undefined && value !== null) {
-      const validator = validators[rules.type]
-      if (!validator(value)) {
-        errors.push(`Invalid type for field: ${key}, expected ${rules.type}`)
-      }
-
       if (rules.type === 'array' && rules.itemsType) {
-        value.forEach((item: any, index: number) => {
-          const itemValidator = validators[rules.itemsType!]
-          if (!itemValidator(item)) {
-            errors.push(
-              `Invalid type for item at index ${index} in field: ${key}, expected ${rules.itemsType}`,
-            )
-          }
-        })
+        if (!Array.isArray(value)) {
+          errors.push(`Invalid type for field: ${key}, expected array`)
+        } else {
+          // Utilizar map para iterar sobre el array
+          value.map((item: any, index: number) => {
+            const itemValidator = validators[rules.itemsType!]
+            if (!itemValidator(item)) {
+              errors.push(
+                `Invalid type for item at index ${index} in field: ${key}, expected ${rules.itemsType}`,
+              )
+            }
+          })
+        }
+      } else {
+        const validator = validators[rules.type]
+        if (!validator(value)) {
+          errors.push(`Invalid type for field: ${key}, expected ${rules.type}`)
+        }
       }
     }
   }
